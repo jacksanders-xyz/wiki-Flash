2b9f575e2b9ebf7dd6a1ee053ef7e94894903aeb
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2022-02-07 17:38:34 -0500

    the card popup works as expected

--- lua/wiki-flash/init.lua
+++ lua/wiki-flash/init.lua
@@ -1,20 +1,87 @@
+local popup = require("plenary.popup")
 local fn = vim.fn
 local api = vim.api
-local function createFloatingWindow()
-  local stats = api.nvim_list_uis()[1]
-  local width = stats.width
-  local height = stats.width
-  local bufh = api.nvim_create_buf(false, true)
-  local winId = api.nvim_open_win(bufh, true, {
-    relative="editor",
-    width = width - 4,
-    height = height - 4,
-    col = 2,
-    row = 2,
-  })
-  api.nvim_buf_set_lines(bufh, 0, -1, true, { "hello world", "heeeyyy" })
-  print("Window size:", height, width)
+function createFloatingWindow()
+    local width = 60
+    local height = 10
+    local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    local bufnr = api.nvim_create_buf(false, false)
+    local Card_win_id, win = popup.create(bufnr, {
+        title = "Card",
+        highlight = "CardWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minheight = height,
+        minwidth = width,
+        borderchars = borderchars,
+    })
+
+    api.nvim_win_set_option(
+        win.border.win_id,
+        "winhl",
+        "Normal:CardBorder"
+    )
+
+    return {
+        bufnr = bufnr,
+        win_id = Card_win_id,
+    }
 end
+
+local function close_card_menu()
+    api.nvim_win_close(Card_win_id, true)
+    Card_win_id = nil
+    Card_bufh = nil
+end
+
+local function toggle_card()
+    if Card_win_id ~= nil and api.nvim_win_is_valid(Card_win_id) then
+        close_menu()
+        return
+    end
+
+    -- if Card_win_id ~= nil then
+    --     open_menu()
+    --     return
+    -- end
+
+    local win_info = createFloatingWindow()
+    local contents = {}
+    -- contents[1] = card_id
+
+    Card_win_id = win_info.win_id
+    Card_bufh = win_info.bufnr
+
+    api.nvim_win_set_option(Card_win_id, "number", true)
+    api.nvim_buf_set_name(Card_bufh, "card_menu")
+    api.nvim_buf_set_lines(Card_bufh, 0, #contents, false, contents)
+    api.nvim_buf_set_option(Card_bufh, "filetype", "vimwiki")
+    -- api.nvim_buf_set_option(Card_bufh, "buftype", "acwrite")
+    api.nvim_buf_set_option(Card_bufh, "bufhidden", "delete")
+    api.nvim_buf_set_keymap(
+        Card_bufh,
+        "n",
+        "q",
+        ":lua toggle_fwin()<CR>",
+        { silent = true }
+    )
+end
+
+-- local function createFloatingWindow()
+--   local stats = api.nvim_list_uis()[1]
+--   local width = stats.width
+--   local height = stats.width
+--   local bufh = api.nvim_create_buf(false, true)
+--   local winId = api.nvim_open_win(bufh, true, {
+--     relative="editor",
+--     width = width - 4,
+--     height = height - 4,
+--     col = 2,
+--     row = 2,
+--   })
+--   api.nvim_buf_set_lines(bufh, 0, -1, true, { "hello world", "heeeyyy" })
+--   print("Window size:", height, width)
+-- end
 
 local function onResize()
   local stats = api.nvim_list_uis()[1]
@@ -25,6 +92,8 @@ end
 
 return {
   createFloatingWindow = createFloatingWindow,
+  close_card_menu = close_card_menu,
+  toggle_card = toggle_card,
   onResize = onResize
 }
 
--- plugin/wiki-flash.vim
+++ plugin/wiki-flash.vim
@@ -1,6 +1,7 @@
 fun! WikiFlash()
-  lua for k in pairs(package.loaded) do if k:match("^wiki%-flash%") then package.loaded[k] = nil end end
-  lua require("wiki-flash").createFloatingWindow()
+  lua for k in pairs(package.loaded) do if k:match("^wiki%-flash") then package.loaded[k] = nil end end
+  " lua require("wiki-flash").createFloatingWindow()
+  lua require("wiki-flash").toggle_card()
 endfun
 augroup WikiFlash
   autocmd!
